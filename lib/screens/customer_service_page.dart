69ac86b03a416aab5981a8aff99d8842f5f24a46
김민영 <kmy@Mac.localdomain>
2025-07-08 14:13:33 +0900

    customer_service_completed

--- lib/screens/customer_service_page.dart
+++ lib/screens/customer_service_page.dart
@@ -4,8 +4,19 @@ import 'package:url_launcher/url_launcher.dart';
 class CustomerServicePage extends StatelessWidget {
   const CustomerServicePage({Key? key}) : super(key: key);
 
-  //QR코드 URL
+  // QR코드 URL
   final String _qrcodeUrl = 'https://open.kakao.com/me/parkourspotcustomer';
+
+  Future<void> _launchKakao(BuildContext context) async {
+    final uri = Uri.parse(_qrcodeUrl);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('링크를 열 수 없습니다.')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +44,6 @@ class CustomerServicePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // 이메일 주소
             const Text(
               'ahco8766@gmail.com',
               style: TextStyle(
@@ -43,8 +53,6 @@ class CustomerServicePage extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 20),
-
-            // "or" 텍스트
             Text(
               'or',
               style: TextStyle(
@@ -54,8 +62,6 @@ class CustomerServicePage extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 20),
-
-            // 카카오톡 문의 텍스트
             const Text(
               '카카오톡 문의',
               style: TextStyle(
@@ -66,24 +72,34 @@ class CustomerServicePage extends StatelessWidget {
             ),
             const SizedBox(height: 30),
 
-            const Text(
-              'https://open.kakao.com/me/parkourspotcustomer',
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-                color: Colors.black54,
+            // 클릭 가능한 링크로 대체
+            GestureDetector(
+              onTap: () => _launchKakao(context),
+              child: const Text(
+                'https://open.kakao.com/me/parkourspotcustomer',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+
+                  color: Colors.blue,
+                ),
               ),
-              textAlign: TextAlign.center,
             ),
             const SizedBox(height: 10),
+
             // QR 코드 이미지
-             Image.asset('assets/QRcode/CustomerService_QRcode.jpeg'),
+            GestureDetector(
+              onTap: () => _launchKakao(context),
+              child: Image.asset(
+                'assets/QRcode/CustomerService_QRcode.jpeg',
+                width: 200,
+                height: 200,
+              ),
+            ),
+
             const SizedBox(height: 30),
-
-
-
           ],
-
         ),
       ),
     );
