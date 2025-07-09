c226bf1d34908f6978addd49577af660274a265f
김민영 <kmy@gimmin-yeong-ui-MacBookPro.local>
2025-07-09 18:41:10 +0900

    excepapikey

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDkvnDPXJ7XzRRywSgK806uOC-V2on2ews")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
