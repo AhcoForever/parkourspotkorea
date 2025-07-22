ccf5e3d5d1e7676220421ce391fb13e3f9a890f9
김민영 <kmy@gimmin-yeong-ui-MacBookPro.local>
2025-07-22 21:02:15 +0900

    signup/out

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,8 +1,9 @@
-import 'dart:ui';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:parkourspotkorea/screens/login_page.dart';
+
+import '../screens/map_page.dart';
 
 class AuthService {
   //회원가입
@@ -14,15 +15,16 @@ class AuthService {
       );
     } on FirebaseAuthException catch (e) {
       String message = '';
+
       if (e.code == 'weak-password') {
         message = 'The password provided is too weak.';
       } else if (e.code == 'email-already-in-use') {
         message = 'An account already exists with that email.';
       }
-
+      //print(e.code);
 
       Fluttertoast.showToast(
-          msg:message,
+        msg: message,
         toastLength: Toast.LENGTH_LONG,
         gravity: ToastGravity.SNACKBAR,
         backgroundColor: Colors.black54,
@@ -30,5 +32,52 @@ class AuthService {
         fontSize: 14.0,
       );
     } catch (e) {}
+  }
+
+  //로그인
+  Future<void> signIn({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      await Future.delayed(const Duration(seconds: 1));
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => MapPage()),
+      );
+    } on FirebaseAuthException catch (e) {
+      String message = '';
+      if (e.code == 'user-not-found') {
+        message = 'No user found for that email.';
+      } else if (e.code == 'wrong-password') {
+        message = 'Wrong password provided for that user.';
+      }
+      //print(e.code);
+
+      Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.SNACKBAR,
+        backgroundColor: Colors.black54,
+        textColor: Colors.white,
+        fontSize: 14.0,
+      );
+    } catch (e) {}
+  }
+
+  //로그아웃
+  Future<void> signOut({required BuildContext context}) async {
+    await FirebaseAuth.instance.signOut();
+    await Future.delayed(const Duration(seconds: 1));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (BuildContext context) => LoginPage()),
+    );
   }
 }
