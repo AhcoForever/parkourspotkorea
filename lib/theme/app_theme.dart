020cb2499adf92d9b16d89ead9522f4d67b55e3a
김민영 <kmy@gimmin-yeong-ui-MacBookPro.local>
2025-07-31 02:33:38 +0900

    createThemeclass

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get light => ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      brightness: Brightness.light,
+
+      //primary 색상군
+      seedColor: Color(0xFF0064FF),
+      onPrimary: Colors.white,
+      //Primary 텍스트/아이콘 색
+      primaryContainer: Color(0xFF83B4F9),
+      onPrimaryContainer: Color(0xFFD6DADF),
+
+    ).copyWith(secondary: Color(0xFF202632)),
+    fontFamily: 'Pretendard',
+
+    ///ElevatedButton 테마
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xFF0064FF),
+        foregroundColor: Colors.white,
+        textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        padding: EdgeInsets.symmetric(horizontal: 17, vertical: 12),
+      ),
+    ),
+
+    //Text 테마
+    textTheme: TextTheme(
+      displayLarge: TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.bold,
+      ),
+      displaySmall: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+      headlineMedium: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+      bodyLarge: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+      bodySmall: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+      labelLarge: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+    ),
+
+    //텍스트 버튼 테마
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: Colors.grey[400],
+        textStyle: const TextStyle(
+          fontSize: 13,
+          fontWeight: FontWeight.w400,
+        ),
+        padding: EdgeInsets.zero,
+        minimumSize: Size.zero,
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      ),
+    ),
+
+    //다이얼로그 테마
+    dialogTheme: DialogThemeData(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      backgroundColor: Colors.white,
+      elevation: 4,
+      titleTextStyle: const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        color: Colors.black87,
+      ),
+      contentTextStyle: TextStyle(fontSize: 14, color: Colors.grey[600]),
+      alignment: Alignment.center,
+      actionsPadding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+    ),
+  );
+}
