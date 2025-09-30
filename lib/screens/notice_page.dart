e49f51f211db45e0b402e55fbe59882fe9daeb5a
kmy <ahco8766@gmail.com>
2025-09-30 17:48:59 +0900

    add_notice_page

---
+++ lib/screens/notice_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:parkourspotkorea/theme/app_colors.dart';
+
+class NoticePage extends StatelessWidget {
+  const NoticePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        backgroundColor: BrandColors.c900,
+      ),
+    );
+  }
+}
